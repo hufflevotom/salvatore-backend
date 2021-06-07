6b671324b9bcce844ff80420750b5e3eece82524
hufflevotom <ten.crypto@outlook.com>
2021-06-07 06:51:19 -0500

    Base de datos

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.get('/', function(req, res){
+    res.send('Salvatore-Project')
+})
 app.use("/v1/vehiculo", require('./routes/vehiculo.routes'));
 app.use("/v1/usuario", require('./routes/usuario.routes'));
 app.use("/v1/responsable", require('./routes/responsable.routes'));
--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = 'mongodb+srv://PSalvatore:P1980sl.@cluster-salvatore.me3xc.mongodb.net/Cluster-Salvatore';
 mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
