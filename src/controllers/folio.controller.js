323ef828e3e21d1b83bca66cbd35febe01921060
hufflevotom <ten.crypto@outlook.com>
2021-06-21 01:29:05 -0500

    Validaciones Folios

--- src/controllers/folio.controller.js
+++ src/controllers/folio.controller.js
@@ -44,7 +44,6 @@ folioController.getFolios = async(req, res) => {
     res.send(folio)
 };
 folioController.createFolio = async(req, res) => {
-    //TODO: Validar el numero de folio 
     const errores = [];
     const { numeroFolio, ruta, nombre, dni, telefono, direccion, fechaEntrega, latitud, longitud, distrito, ordenEntrega, inicioVisita, finVisita, descripcionPedido, localAbastecimiento } = req.body;
     const folio = await Folio.findOne({ numeroFolio: req.body.numeroFolio });
@@ -135,18 +134,61 @@ folioController.getFolio = async(req, res) => {
     res.send(r)
 };
 folioController.updateFolio = async(req, res) => {
+    const errores = [];
     const { numeroFolio, ruta, nombre, dni, telefono, direccion, fechaEntrega, latitud, longitud, distrito, ordenEntrega, inicioVisita, finVisita, descripcionPedido, localAbastecimiento } = req.body;
     const folio = await Folio.findOne({ _id: req.params.id })
-    const detalleentrega = await DetalleEntrega.findOne({ _id: folio.idDetalleEntrega })
-    await UbicacionEntrega.findByIdAndUpdate(detalleentrega.idUbicacionEntrega, { latitud, longitud, distrito })
-    await HorarioVisita.findByIdAndUpdate(detalleentrega.idHorarioVisita, { inicioVisita, finVisita })
-    await DetalleCliente.findByIdAndUpdate(folio.idDetalleCliente, { nombre, dni, telefono, direccion })
-    await DetallePedido.findByIdAndUpdate(folio.idDetallePedido, { descripcionPedido })
-    await LocalAbastecimiento.findByIdAndUpdate(folio.idLocalAbastecimiento, { localAbastecimiento })
-    await DetalleEntrega.findByIdAndUpdate(folio.idDetalleEntrega, { fechaEntrega, ordenEntrega })
-    await Folio.findByIdAndUpdate(req.params.id, { numeroFolio, ruta })
-        //TODO: Validar numero de folio
-    res.send({ type: 'success', message: 'Folio actualizado' })
+    if (folio) {
+        if (numeroFolio) {
+            if (!/^[0-9]+$/.test(numeroFolio)) {
+                errores.push({ message: 'El numero de folio debe ser válido' })
+            }
+        }
+        if (ruta) {
+            if (!/^[0-9]+$/.test(ruta)) {
+                errores.push({ message: 'La ruta debe ser válida' })
+            }
+        }
+        if (dni) {
+            if (!/^\d{8}(?:[-\s]\d{4})?$/.test(dni)) {
+                errores.push({ message: 'El DNI debe tener 8 números' })
+            }
+        }
+        if (ordenEntrega) {
+            if (!/^[0-9]$/.test(ordenEntrega)) {
+                errores.push({ message: 'La orden de entrega debe ser válida' })
+            }
+        }
+        if (inicioVisita) {
+            if (!/^[0-9]+$/.test(inicioVisita)) {
+                errores.push({ message: 'El inicio de visita debe ser válido' })
+            }
+        }
+        if (finVisita) {
+            if (!/^[0-9]+$/.test(finVisita)) {
+                errores.push({ message: 'El fin de visita debe ser válido' })
+            }
+        }
+        if (nombre) {
+            if (!/^[a-zA-ZáÁéÉíÍóÓúÚñÑüÜ\s]+$/.test(nombre)) {
+                errores.push({ message: 'El nombre debe ser válido' })
+            }
+        }
+    } else {
+        errores.push({ message: 'El folio ' + numeroFolio + ' no existe' })
+    }
+    if (errores.length > 0) {
+        res.send({ type: 'error', errores })
+    } else {
+        const detalleentrega = await DetalleEntrega.findOne({ _id: folio.idDetalleEntrega })
+        await UbicacionEntrega.findByIdAndUpdate(detalleentrega.idUbicacionEntrega, { latitud, longitud, distrito })
+        await HorarioVisita.findByIdAndUpdate(detalleentrega.idHorarioVisita, { inicioVisita, finVisita })
+        await DetalleCliente.findByIdAndUpdate(folio.idDetalleCliente, { nombre, dni, telefono, direccion })
+        await DetallePedido.findByIdAndUpdate(folio.idDetallePedido, { descripcionPedido })
+        await LocalAbastecimiento.findByIdAndUpdate(folio.idLocalAbastecimiento, { localAbastecimiento })
+        await DetalleEntrega.findByIdAndUpdate(folio.idDetalleEntrega, { fechaEntrega, ordenEntrega })
+        await Folio.findByIdAndUpdate(req.params.id, { numeroFolio, ruta })
+        res.send({ type: 'success', message: 'Folio actualizado' })
+    }
 };
 folioController.deleteFolio = async(req, res) => {
     const folio = await Folio.findOne({ _id: req.params.id })
